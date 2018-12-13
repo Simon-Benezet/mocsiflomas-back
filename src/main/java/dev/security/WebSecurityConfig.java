55126031d24631ab366330dacc28b1958949104c
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-12-13 16:13:54 +0100

    a

--- src/main/java/dev/security/WebSecurityConfig.java
+++ src/main/java/dev/security/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 // toutes les requêtes doivent être authentifiées
                 .authorizeRequests()
-                .antMatchers("/collegue/upload/**","/images/**","/produits/**","/collegue/nouveau")
+                .antMatchers("/collegue/upload/**","/images/**","/produits/**","/collegue/nouveau", "/{nomFigurine}")
                 .permitAll().anyRequest().authenticated()
                 .and()
                 // génération d'un formulaire de login
