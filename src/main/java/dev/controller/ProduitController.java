3962edf9e2a19cc69d73fecdee3f964dd7fc49bc
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-12-17 09:59:35 +0100

    ok

--- src/main/java/dev/controller/ProduitController.java
+++ src/main/java/dev/controller/ProduitController.java
@@ -26,57 +26,13 @@ public class ProduitController {
 
 	@Autowired
 	private ProduitRepo produitRepo;
-
-
-@CrossOrigin
-@RestController()
-@RequestMapping()
-public class ProduitController {
-
-	@Autowired
-	private ProduitRepo produitRepo;
 	
 	@GetMapping("/produits")
 	public List<Produit> findAll() {
 		return this.produitRepo.findAll();
 	}
 	
-	@Secured(value = { "ROLE_ADMINISTRATEUR" })
-	@PostMapping
-	public Produit createProduit(@RequestBody Produit ajoutProd) {
-		Produit ajPro = new Produit();
-		ajPro.setNomSaga(ajoutProd.getNomSaga());
-		ajPro.setNomImage(ajoutProd.getNomImage());
-		ajPro.setPersonnage(ajoutProd.getPersonnage());
-		ajPro.setNomFigurine(ajoutProd.getNomFigurine());
-		ajPro.setTaille(ajoutProd.getTaille());
-		ajPro.setDescription(ajoutProd.getDescription());
-		ajPro.setNumeroFigurine(ajoutProd.getNumeroFigurine());
-		this.produitRepo.save(ajPro);
-		return ajPro;
-	}
 	
-	// Modifier Les produits 
-	@PatchMapping("/{modif-produit}")
-    public Produit modif(@PathVariable String nomFigurine, @RequestBody Produit prod) {
-
-		Produit produit = this.produitRepo.findByNomFigurine(nomFigurine);
-
-		produit.setNomFigurine(prod.getNomFigurine());
-		produit.setNomImage(prod.getNomImage());
-		produit.setNomSaga(prod.getNomSaga());
-		produit.setNumeroFigurine(prod.getNumeroFigurine());
-		produit.setPersonnage(prod.getPersonnage());
-		produit.setPrix(prod.getPrix());
-		produit.setTaille(prod.getTaille());
-		produit.setDescription(prod.getDescription());
-		
-		this.produitRepo.save(produit);    	
-		return produit;
-	}
-	
-	@GetMapping("/{nomFigurine}")
-
 	// Modifier Les produits
 	@Secured(value = { "ROLE_ADMINISTRATEUR" })
 	@PatchMapping("/{nomFigurine}")
@@ -128,31 +84,7 @@ public class ProduitController {
 		
 	}
 
-	// filtre
-	@GetMapping("/recherche")
-	public List<Produit> recherche(@RequestParam String nomSaga, @RequestParam String personnage) {
-
-		// Recherche solo la saga
-
-		if (!nomSaga.isEmpty() && personnage.isEmpty()) {
-			return produitRepo.findAll(Recherche.triSaga(nomSaga));
-
-		}
-
-		// recherche solo le personnage
-		if (nomSaga.isEmpty() && !personnage.isEmpty()) {
-			return produitRepo.findAll(Recherche.triPersonnage(personnage));
-		}
-		// recherche les deux
-		if (!nomSaga.isEmpty() && !personnage.isEmpty()) {
-			List<Produit> l = new ArrayList<Produit>();
-			l.addAll(produitRepo.findAll(Recherche.triPersonnage(personnage)));
-			l.addAll(produitRepo.findAll(Recherche.triSaga(nomSaga)));
-			return l;
-		} else {
-			return null;
-		}
-
+	
 	}
 
-}
+
